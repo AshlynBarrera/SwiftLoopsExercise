1f80ab31e8380f1905a3d7db2b7a3d949f8445e1
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-09 09:17:21 -0600

    Initial Commit

---
+++ swiftLoopsExercise.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+
+//SwiftLoopsExercise
+
+//1
+print("question 1")
+for i in 1...7{
+    print(i)
+}
+
+//2a
+print("question 2")
+let greetings = ["Hello", "Hi", "Whats up"];
+
+
+//2b
+for i in 0..<3{
+    print(greetings[i])
+}
+
+//3a
+print("question 3")
+var num1 = 5
+
+//3b
+
+while (num1 <= 25){
+    print(num1)
+    num1 = num1 + 4
+}
+
+//4
+// the loop with only run once
+
